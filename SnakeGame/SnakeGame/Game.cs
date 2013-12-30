1181e326ed5432f047832001a65a346749dc7a61
zaq007 <sergey4000@tut.by>
2013-12-30 20:47:49 +0300

    test

---
+++ SnakeGame/SnakeGame/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace SnakeGame
+{
+    public class Game
+    {
+        public Game()
+        {
+           
+        }
+
+        public string Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spritePatch)
+        {
+
+        }
+
+    }
+}
---
+++ SnakeGame/SnakeGame/Objects/GameObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace SnakeGame.Objects
+{
+    class GameObject
+    {
+        public Texture2D Texture;
+        public Vector2 Position;
+        public Vector2 Speed;   
+        public Vector2 Velocity;
+        public bool Visibility; 
+                                
+
+        public GameObject(Texture2D texture, Vector2 position)
+        {
+
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {        }
+
+        public virtual void Draw(GameTime gameTime)
+        {        }
+
+    }
+}
